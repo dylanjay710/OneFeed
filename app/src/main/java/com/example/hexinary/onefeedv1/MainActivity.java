7f58b368ef0222af513c0e754fabaecf46dd7a5d
professionalcodes <dylan.dannenhauer@protonmail.com>
2017-03-28 01:04:29 -0600

    added createAccount class, added it in the android manifest and click handler for create account btn

--- app/src/main/java/com/example/hexinary/onefeedv1/MainActivity.java
+++ app/src/main/java/com/example/hexinary/onefeedv1/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.hexinary.onefeedv1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 //import com.facebook.FacebookSdk;
 
 
@@ -11,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void oneFeedClick(View v) {
+        Intent intent = new Intent(getApplicationContext(), CreateAccount.class);
+        startActivity(intent);
     }
 }
