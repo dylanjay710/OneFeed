13bca13ec12c62602fe2e2b1070c0d9341c8a157
professionalcodes <dylan.dannenhauer@protonmail.com>
2017-03-28 03:04:34 -0600

    added account_creation input boxes and added ProUtils.java for utility functions

--- app/src/main/java/com/example/hexinary/onefeedv1/CreateAccount.java
+++ app/src/main/java/com/example/hexinary/onefeedv1/CreateAccount.java
@@ -2,6 +2,7 @@ package com.example.hexinary.onefeedv1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class CreateAccount extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class CreateAccount extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        System.out.println("On creation method of Create Account page");
     }
+
 }
+
+//        System.out.println("Testing click hander");
+//        System.out.println(v.getId());
+//        ProUtils.getInstance().log("sup bich");
---
+++ app/src/main/java/com/example/hexinary/onefeedv1/ProUtils.java
@@ -0,0 +1,31 @@
+package com.example.hexinary.onefeedv1;
+
+/**
+ * Created by hexinary on 17-3-28.
+ */
+
+/* DD */
+public class ProUtils {
+
+    private static ProUtils _instance;
+
+    private ProUtils() {
+
+    }
+
+    public static ProUtils getInstance() {
+        if (_instance == null) {
+            _instance = new ProUtils();
+        }
+        return _instance;
+    }
+//    private  ProUtils getInstance() {
+//        return this.proUtilsInstance;
+//    }
+
+    public void log(String msg) {
+        System.out.println(msg);
+    }
+
+
+}
