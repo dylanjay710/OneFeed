38e3fb475230da6a4413e5deb077a894a4e9bd54
professionalcodes <dylan.dannenhauer@protonmail.com>
2017-04-01 01:18:13 -0600

    used google default provided button to load google feed opposed to custom google button

--- app/src/main/java/com/example/hexinary/onefeedv1/UserProfile.java
+++ app/src/main/java/com/example/hexinary/onefeedv1/UserProfile.java
@@ -1,11 +1,17 @@
 package com.example.hexinary.onefeedv1;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 
-public class UserProfile extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+public class UserProfile extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +32,15 @@ public class UserProfile extends AppCompatActivity {
     public void googleAuthorize(View v) {
         ProUtils.getInstance().log("authorizing google");
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        GoogleApiClient mobileGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
     }
 
 
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
